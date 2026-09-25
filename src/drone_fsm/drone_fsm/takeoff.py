789ada988798a55e3d214382fa051578f5e0f062
Sonavelsaker <sindreon@stud.ntnu.no>
2026-09-25 15:30:35 +0200

    fix: minor bugs

--- src/drone_fsm/drone_fsm/takeoff.py
+++ src/drone_fsm/drone_fsm/takeoff.py
@@ -21,13 +21,10 @@ class TakeoffNode(rclpy.node.Node):
         self.trajectorySetpointTimer = self.create_timer(self.trajectory_period, self.trajectorySetpoint_callback)
         self.vehicleCommandTimer = self.create_timer(self.vehicle_command_period, self.vehicleCommand_callback)
 
-        self.pos_subscriber = self.create_subscription(VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.update_vehicle_pos)
-        self.status_subscriber = self.create_subscription(VehicleStatus, '/fmu/out/vehicle_status', self.update_vehicle_status)
+        self.pos_subscriber = self.create_subscription(VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.update_vehicle_pos,10)
+        self.status_subscriber = self.create_subscription(VehicleStatus, '/fmu/out/vehicle_status', self.update_vehicle_status,10)
         self.arming_state = VehicleStatus.ARMING_STATE_DISARMED # Taken from github
         self.nav_state = VehicleStatus.NAVIGATION_STATE_MAX # Taken from github
-
-        self.has_been_told_to_go_up = False
-        self.last_pos_msg = None
 
         self.is_first_pos_msg = True
         self.target = [0,0,-3]
@@ -43,28 +40,20 @@ class TakeoffNode(rclpy.node.Node):
         self.offboardControlPublisher_.publish(ocm_msg)
 
     def trajectorySetpoint_callback(self):
-        if not (self.nav_state == VehicleStatus.NAVIGATION_STATE_OFFBOARD and self.arming_state == VehicleStatus.ARMING_STATE_ARMED and (not self.has_been_told_to_go_up)):
-            return
         if self.last_pos_msg is None:
             return
+
         ts_msg = TrajectorySetpoint()
         ts_msg.timestamp = self.get_clock().now().nanoseconds // 1000
-        ts_msg.position[0] = self.target[0]
-        ts_msg.position[1] = self.target[1]
-        ts_msg.position[2] = self.target[2]
+        ts_msg.position = [0.0, 0.0, -3.0]
+        ts_msg.yaw = 0.0
 
         self.trajectorySetpointPublisher_.publish(ts_msg)
 
     def vehicleCommand_callback(self):
         vc_msg = VehicleCommand()
 
-        if (self.arming_state == VehicleStatus.ARMING_STATE_DISARMED):
-            vc_msg.param1 = 1 # 1 is arm, 0 is unarm
-            vc_msg.command = VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM # Constant 1
-            self.vehicleCommandPublisher_.publish(vc_msg)
-            return
-
-        elif (self.nav_state != VehicleStatus.NAVIGATION_STATE_OFFBOARD):
+        if (self.nav_state != VehicleStatus.NAVIGATION_STATE_OFFBOARD):
             vc_msg.command = VehicleCommand.VEHICLE_CMD_DO_SET_MODE
             vc_msg.param1 = 1.0
             vc_msg.param2 = 6.0
@@ -78,6 +67,21 @@ class TakeoffNode(rclpy.node.Node):
 
             self.vehicleCommandPublisher_.publish(vc_msg)
             return
+        
+        elif (self.arming_state == VehicleStatus.ARMING_STATE_DISARMED):            
+            vc_msg.command = VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM
+            vc_msg.param1 = 1.0
+
+            vc_msg.target_system = 1
+            vc_msg.target_component = 1
+            vc_msg.source_system = 1
+            vc_msg.source_component = 1
+            vc_msg.from_external = True
+            vc_msg.timestamp = self.get_clock().now().nanoseconds // 1000
+
+            self.vehicleCommandPublisher_.publish(vc_msg)
+            return
+
 
 
 
