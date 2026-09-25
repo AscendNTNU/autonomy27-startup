4631f5c236629163d0959fdbeef210ede84099a7
Sverreabo <sverreabo@gmail.com>
2026-09-25 16:35:16 +0200

    fix: improve landing so we don't crash

--- src/drone_fsm/drone_fsm/landing_node.py
+++ src/drone_fsm/drone_fsm/landing_node.py
@@ -2,14 +2,16 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
 
-from px4_msgs.msg import TrajectorySetpoint, VehicleLocalPosition, VehicleCommand
+from px4_msgs.msg import TrajectorySetpoint, VehicleCommand, OffboardControlMode, VehicleLandDetected
 from std_srvs.srv import Trigger
 
-import numpy as np
+
+NaN = float('nan')
 
 class LandingNode(Node):
     def __init__(self):
         super().__init__("landing_node")
+        self.is_landing = False
 
         qos_profile = QoSProfile(
             reliability=QoSReliabilityPolicy.BEST_EFFORT,
@@ -18,16 +20,21 @@ class LandingNode(Node):
             depth=1
         )
 
+        self.offboard_publisher = self.create_publisher(
+            OffboardControlMode, '/fmu/in/offboard_control_mode', qos_profile)
+
         self.trajectory_publisher = self.create_publisher(
             TrajectorySetpoint, '/fmu/in/trajectory_setpoint', qos_profile)
 
         self.vehicle_command_publisher = self.create_publisher(
             VehicleCommand, '/fmu/in/vehicle_command', qos_profile)
 
-        self.position_subscriber = self.create_subscription(
-            VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.position_callback, qos_profile)
+        self.land_detected_subscriber = self.create_subscription(
+            VehicleLandDetected, '/fmu/out/vehicle_land_detected', self.land_detected_callback, qos_profile)
 
         self.land_service = self.create_service(Trigger, '/land', self.land_service_callback)
+
+        self.get_logger().info("Init ferdig")
 
     def land_service_callback(self, request, response):
         self.get_logger().info("Mottatt /land trigger. Starter landing.")
@@ -39,41 +46,59 @@ class LandingNode(Node):
         return response
 
     def start_landing(self):
-        timer_period = 0.25  # seconds
+        self.is_landing = True
+        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.get_logger().info("Heartbeat laget")
 
     def timer_callback(self):
+        self.publish_offboard()
+        self.publish_setpoint()
+
+    def publish_setpoint(self):
         setpoint = TrajectorySetpoint()
         setpoint.timestamp = self.get_timestamp()
 
         # NED
-        setpoint.position = [0.0, 0.0, 0.0]
+        setpoint.position = [NaN, NaN, NaN]
+        setpoint.velocity = [0.0, 0.0, 0.5]
+        setpoint.yaw = NaN
 
         self.trajectory_publisher.publish(setpoint)
-        self.get_logger().info(
-            f"Publiserer til TrajectorySetpoint: {setpoint.position}")
 
-    def position_callback(self, msg):
-        position = np.array([msg.x, msg.y, msg.z])
-        distance = np.linalg.norm(position)
+    def publish_offboard(self):
+        msg = OffboardControlMode()
+        msg.timestamp = self.get_timestamp()
+        msg.position = False
+        msg.velocity = True
+        msg.acceleration = False
+        msg.attitude = False
+        msg.body_rate = False
+        
+        self.offboard_publisher.publish(msg)
 
-        self.get_logger().info(
-            f"Mottatt VehicleLocalPosition: {position}. Avstand: {distance:.3f}")
-
-        threshold = 0.1  # 10 cm
-        if distance < threshold:
+    def land_detected_callback(self, msg):
+        if self.is_landing and msg.landed:
             self.landing_finished()
         
     def landing_finished(self):
-        self.timer.destroy()
-        self.get_logger().info("Stoppet timer")
-        self.disarm()
+        self.is_landing = False
+        if self.timer is not None:
+            self.timer.destroy()
+            self.timer = None
+            self.get_logger().info("Stoppet timer")
+            self.disarm()
 
     def disarm(self):
         msg = VehicleCommand()
         msg.command = VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM
         msg.param1 = 0.0
+        msg.param2 = 0.0
+        msg.param3 = 0.0
+        msg.param4 = 0.0
+        msg.param5 = 0.0
+        msg.param6 = 0.0
+        msg.param7 = 0.0
         msg.target_system = 1
         msg.target_component = 1
         msg.source_system = 1
