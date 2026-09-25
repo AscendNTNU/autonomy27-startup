65f96345bae8c711a4636b110141364f588398f4
Sverreabo <sverreabo@gmail.com>
2026-09-25 14:17:33 +0200

    feat: improve landing_node

--- src/drone_fsm/drone_fsm/landing_node.py
+++ src/drone_fsm/drone_fsm/landing_node.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
 
 from px4_msgs.msg import TrajectorySetpoint, VehicleLocalPosition, VehicleCommand
+from std_srvs.srv import Trigger
 
 import numpy as np
 
@@ -19,14 +20,28 @@ class LandingNode(Node):
 
         self.trajectory_publisher = self.create_publisher(
             TrajectorySetpoint, '/fmu/in/trajectory_setpoint', qos_profile)
-        timer_period = 0.25  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
 
         self.vehicle_command_publisher = self.create_publisher(
             VehicleCommand, '/fmu/in/vehicle_command', qos_profile)
 
         self.position_subscriber = self.create_subscription(
             VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.position_callback, qos_profile)
+
+        self.land_service = self.create_service(Trigger, '/land', self.land_service_callback)
+
+    def land_service_callback(self, request, response):
+        self.get_logger().info("Mottatt /land trigger. Starter landing.")
+        self.start_landing()
+
+        response.success = True
+        response.message = ""
+
+        return response
+
+    def start_landing(self):
+        timer_period = 0.25  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.get_logger().info("Heartbeat laget")
 
     def timer_callback(self):
         setpoint = TrajectorySetpoint()
@@ -41,12 +56,13 @@ class LandingNode(Node):
 
     def position_callback(self, msg):
         position = np.array([msg.x, msg.y, msg.z])
+        distance = np.linalg.norm(position)
 
         self.get_logger().info(
-            f"Mottatt VehicleLocalPosition: {position}")
+            f"Mottatt VehicleLocalPosition: {position}. Avstand: {distance:.3f}")
 
         threshold = 0.25  # 25 cm
-        if np.linalg.norm(position) < threshold:
+        if distance < threshold:
             self.landing_finished()
         
     def landing_finished(self):
